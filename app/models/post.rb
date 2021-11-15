bce2b317cbd41918e81e1467960ff8b1e620fb35
arnold <nganizyo@gmail.com>
2021-11-15 15:08:56 +0000

    commit tree 47df8838578ce49750636b4b6f8409f1bf8dc3a1

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,19 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories, reject_if: :all_blank
 
+  def category_attributes=(category_attributes)
+    if !(category_attributes.any?(nil))
+      category_attributes.values.each do |c|
+        category = Category.find_or_create_by(c)
+        self.categories << category
+      end
+    end
+  end
+
+  def unique_commenters
+      self.comments ? self.comments.collect {|c| c.user}.uniq : []
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+@category = Category.create(name: "Cool")
+@category1 = Category.create(name: "doom")
+@category2 = Category.create(name: "gloom")
+@post = Post.create(title: "So Awesome", content: "Man am I a great blogger OR WHAT!")
+@post.categories << @category
+@user = User.create(username: 'RealBigFish')
+@comment = Comment.create(content: "This was great!", user: @user, post: @post)
+@post1 = Post.create(title: "Feeling Groovy", content: "I'm feeling so groovy")
+@user1 = User.create(username: 'CoolGuy5')
+@comment1 = @post1.comments.build(content: "Great Post!", user: @user1)
+@comment1.save
+@post1.save
